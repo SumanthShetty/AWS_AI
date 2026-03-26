5532724d76817af06af597cb22584f3f755f4b22
sumanth <smnthshetty10@gmail.com>
2026-03-26 16:17:10 +0530

    Implement Converse API interaction in convers_api.py

---
+++ bedrock_lab/convers/convers_api.py
@@ -0,0 +1,68 @@
+import boto3, json
+
+print("\n----A basic call to the Converse API----\n")
+
+session = boto3.Session()
+bedrock = session.client(service_name='bedrock-runtime')
+
+message_list = []
+
+initial_message = {
+    "role": "user",
+    "content": [
+        { "text": "How are you today?" } 
+    ],
+}
+
+message_list.append(initial_message)
+
+response = bedrock.converse(
+    modelId="us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+    messages=message_list,
+    inferenceConfig={
+        "maxTokens": 2000,
+        "temperature": 0
+    },
+)
+
+response_message = response['output']['message']
+print(json.dumps(response_message, indent=4))
+
+print("\n----Alternating user and assistant messages----\n")
+
+message_list.append(response_message)
+
+print(json.dumps(message_list, indent=4))
+
+print("\n----Setting a system prompt----\n")
+
+summary_message = {
+    "role": "user",
+    "content": [
+        { "text": "Can you please summarize our conversation so far?" } 
+    ],
+}
+
+message_list.append(summary_message)
+
+response = bedrock.converse(
+    modelId="us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+    messages=message_list,
+    system=[
+        { "text": "Please respond to all requests in the style of a pirate." }
+    ],
+    inferenceConfig={
+        "maxTokens": 2000,
+        "temperature": 0
+    },
+)
+
+response_message = response['output']['message']
+print(json.dumps(response_message, indent=4))
+
+message_list.append(response_message)
+
+print("\n----Getting response metadata and token counts----\n")
+
+print("Stop Reason:", response['stopReason'])
+print("Usage:", json.dumps(response['usage'], indent=4))
